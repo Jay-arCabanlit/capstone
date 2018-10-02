d1a40fdc247371a898f3ac06045a39d798f0e59e
sanzmoses <sanzmoses@gmail.com>
2018-10-02 17:49:42 +0800

    with validation and etc

--- function/sms.php
+++ function/sms.php
@@ -12,11 +12,28 @@ $token = '2520d19d4c22e532585da4b5db3b34e2';
 $client = new Client($sid, $token);
 
 if(isset($_POST['sms'])) {
+	$num = $_POST['number'];
 	$msg = $_POST['message'];
+
+	//validation
+	if(strlen($num) != 14) {
+		$url = $_SERVER['HTTP_REFERER'].'?status=error&message="Length Invalid!"';
+		header("Location: {$url}");
+	}
+	else if(!is_numeric($num)) {
+		$url = $_SERVER['HTTP_REFERER'].'?status=error&message="Invalid Input!"';
+		header("Location: {$url}");
+	}
+
+	if(strlen($msg) == 0){
+		$url = $_SERVER['HTTP_REFERER'].'?status=error&message="No message to send!"';
+		header("Location: {$url}");
+	}
+
 	// Use the client to do fun stuff like send text messages!
 	$client->messages->create(
 	    // the number you'd like to send the message to
-	    '+639074239571',
+	    '+'.$num,
 	    array(
 	        // A Twilio phone number you purchased at twilio.com/console
 	        'from' => '+12193361316',
--- send.php
+++ send.php
@@ -1,16 +1,32 @@
 <html>
 
-	<form action="/function/sms.php" method="POST">
-		<input type="text" name="message" required>
-		<button type="submit" name="sms">Send</button>
-	</form>
+<form action="/function/sms.php" method="POST">
 
-	<?php 
 
-	if(isset($_GET['status'])) {
-		echo '<h1>'.$_GET['status'].'</h1>';
-	} 
+	
+	<p style="font-size: 12px; color: red"> Note: Only registered numbers <br>are available for twilio trial! <br> numbers must start with '63'</p>
 
-	?>
+	<p>Number: </p>
+	<input type="text" name="number" maxlength="14" placeholder="number" required><br>
+
+	<p>Message: </p>
+	<textarea name="message" id="" cols="30" rows="10" maxlength="150"></textarea>
+
+	<button type="submit" name="sms">Send</button><br>	
+
+
+</form>
+
+<?php 
+
+if(isset($_GET['status']) && $_GET['status'] != 'error') {
+	echo '<h1>'.$_GET['status'].'</h1>';
+} 
+
+if(isset($_GET['status']) && $_GET['status'] == 'error') {
+	echo '<h1 style="color:red">'.$_GET['message'].'</h1>';
+}
+
+?>
 
 </html>
